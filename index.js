217d5b0403158e368281f4d2668466c70ce8ef78
Atul Sood <atulsood@pm.me>
2021-10-17 17:28:26 -0300

    changes

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict"; 
+
+let $ = document.querySelector.bind(document);
+
+document.addEventListener('DOMContentLoaded', initialize);
+
+function initialize() {  console.log('DOM loaded'); }
