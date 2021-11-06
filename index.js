1aa9a69912b3a7b46f0bde93837047bab58c0f6a
Atul Sood <atulsood@pm.me>
2021-11-06 19:59:32 -0300

    metamask connectivity and account info demo

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-"use strict"; 
+'use strict';
 
 let $ = document.querySelector.bind(document);
 
-document.addEventListener('DOMContentLoaded', initialize);
+let btnConnect = $('#btnConnect');
+let lblAccountAddress = $('#accountAddress');
+let lblaccountBalance = $('#accountBalance');
 
-function initialize() {  console.log('DOM loaded'); }
+const connect = () => {
+  if (window.ethereum) {
+    window.ethereum
+      .request({ method: 'eth_requestAccounts' })
+      .then((accounts) => {
+        accountChangeHandler(accounts[0]);
+      });
+  } else {
+    console.log('metamask not found');
+  }
+};
+
+btnConnect.addEventListener('click', connect);
+
+const accountChangeHandler = (account) => {
+  lblAccountAddress.innerText = account.toString();
+  getUserBalance(account.toString());
+};
+
+const getUserBalance = (address) => {
+  window.ethereum
+    .request({ method: 'eth_getBalance', params: [address, 'latest'] })
+    .then((balance) => {
+      lblaccountBalance.innerText = ethers.utils.formatEther(balance);
+    });
+};
+
+window.ethereum.on('accountsChanged', accountChangeHandler);
